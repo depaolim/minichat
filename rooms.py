80bf0bf7155d323faca2771e99c95c4acc8a5ef9
Marco De Paoli <depaolim@gmail.com>
2015-11-15 06:35:14 +0100

    rooms test cases

--- rooms.py
+++ rooms.py
@@ -28,3 +28,49 @@ class User(object):
             return self.queue.get(timeout=10)
         except queue.Empty:
             return []
+
+
+################################################################################
+# TESTS
+################################################################################
+
+import unittest
+
+
+class TestAcceptance(unittest.TestCase):
+
+    def setUp(self):
+        self.USERS = {}
+        self.ROOMS = {'python': Room(), 'django': Room()}
+
+    def join(self, room, uid):
+        user = self.USERS.get(uid, None)
+
+        if not user:
+            self.USERS[uid] = user = User()
+
+        active_room = self.ROOMS[room]
+        active_room.subscribe(user)
+        print 'subscribe', active_room, user
+
+        messages = active_room.backlog()
+        return messages
+
+    def put(self, uid, room_name, message):
+        self.USERS[uid]
+        room = self.ROOMS[room_name]
+        room.add(':'.join([uid, message]))
+
+    def poll(self, uid):
+        return self.USERS[uid].get()
+
+    def test(self):
+        self.assertFalse(self.join("python", "me"))
+        self.assertFalse(self.join("python", "io"))
+        self.put("me", "python", "ciao")
+        self.assertEqual(self.poll("io"), "me:ciao")
+        self.assertEqual(self.join("python", "you"), ["me:ciao"])
+
+
+if __name__ == '__main__':
+    unittest.main()
