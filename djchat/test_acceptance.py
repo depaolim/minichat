93f524fcf881e511b87b270cab0c7cbd737cb73d
Marco De Paoli <depaolim@gmail.com>
2015-11-22 09:06:55 +0100

    django first test with admin

--- djchat/test_acceptance.py
+++ djchat/test_acceptance.py
@@ -1,9 +1,25 @@
+import os
 import subprocess
 
 import pytest
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djchat.settings")
+
+
+@pytest.fixture
+def django():
+    print ("setup Django")
+    import django
+    from django.contrib.auth.models import User
+    from django.core.management import call_command
+    django.setup()
+
+    os.remove("db.sqlite3")
+    call_command("migrate")
+    User.objects.create_superuser('admin', 'admin@example.com', 'pass')
 
 
 @pytest.fixture(scope="module")
@@ -24,11 +40,11 @@ def browser(request):
     return browser  # provide the fixture value
 
 
-def test_acceptance(browser):
-    browser.get('http://localhost:5000')
-    assert 'Welcome to Django' in browser.title
+def test_acceptance(django, browser):
+    browser.get('http://localhost:5000/admin')
+    browser.find_element_by_name('username').send_keys('admin')
+    browser.find_element_by_name('password').send_keys('pass' + Keys.RETURN)
     return
-    browser.find_element_by_id('user_id').send_keys('me' + Keys.RETURN)
     browser.find_element_by_link_text('python').click()
     browser.find_element_by_id('messageinput').send_keys('ciao' + Keys.RETURN)
     messages = browser.find_elements_by_css_selector('#messages li')
